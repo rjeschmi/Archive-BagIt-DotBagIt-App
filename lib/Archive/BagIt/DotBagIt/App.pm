f18dbdab39770e7861c1a0c59c5d3528ef9abf57
Robert Schmidt <rjeschmi@gmail.com>
2014-12-01 16:24:10 -0500

    starting to create a usable app

--- lib/Archive/BagIt/DotBagIt/App.pm
+++ lib/Archive/BagIt/DotBagIt/App.pm
@@ -2,27 +2,17 @@ use strict;
 use warnings;
 package Archive::BagIt::DotBagIt::App;
 
+use App::Cmd::Setup -app;
+
 # VERSION
 
 # ABSTRACT: An app to work with a DotBagit bag
 #
 
-use Moo;
-use MooX::Cmd;
-use MooX::Options
-    authors => 'Rob Schmidt <rjeschmi@gmail.com>',
-    description => "A commandline interface to a new DotBagIt type bag",
-    synopsis => "A commandline interface to DotBagIts";
-
-option 'debug' => ( is => 'ro');
-
-sub execute {
-    my ( $self, $args_ref, $chain_ref ) = @_;
-    die ("running without a command");
-}
+#authors => 'Rob Schmidt <rjeschmi@gmail.com>',
+#description => "A commandline interface to a new DotBagIt type bag",
+#synopsis => "A commandline interface to DotBagIts";
 
 
 
 1;
-
-
